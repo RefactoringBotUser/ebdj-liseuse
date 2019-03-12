426f7a1fb81ea7b9a42db7243611a7be714ff866
Nicolas Gendron <nic.gendron@gmail.com>
2019-03-12 21:19:32 +0100

    Correction sur libellé.

--- module/ebdj-commun/src/main/java/fr/qp1c/ebdj/liseuse/commun/bean/anomalie/TypeAnomalie.java
+++ module/ebdj-commun/src/main/java/fr/qp1c/ebdj/liseuse/commun/bean/anomalie/TypeAnomalie.java
@@ -3,7 +3,7 @@ package fr.qp1c.ebdj.liseuse.commun.bean.anomalie;
 public enum TypeAnomalie {
 
 	REPONSE_FAUSSE("Réponse fausse"), REPONSE_INCOMPLETE("Réponse incomplète"), REPONSE_INEXACTE(
-			"Réponse inexacte"), QUESTION_FAUSSE("Réponse fausse"), QUESTION_PERIMEE(
+			"Réponse inexacte"), QUESTION_FAUSSE("Question fausse"), QUESTION_PERIMEE(
 					"Question périmée"), QUESTION_MAL_REDIGEE("Question mal rédigée"), MULTI_ANOMALIE("Autre problème");
 
 	private final String name;
